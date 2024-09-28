8b45183e986c680d38a5308fc1258919c67a0a26
Emanuel <152207479+eeyieryi@users.noreply.github.com>
2024-09-29 00:43:17 +0400

    separate lists view to /lists

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,28 +3,9 @@
 
 	import { page } from '$app/stores';
 
-	import { Plus, ListTodo, Timer } from 'lucide-svelte';
+	import { ListTodo, Timer } from 'lucide-svelte';
 
-	import { Input } from '$lib/components/ui/input';
-	import { Button } from '$lib/components/ui/button';
-
-	type PremadeListItem = {
-		href: string;
-		title: string;
-	};
-
-	const premadeLists: PremadeListItem[] = [
-		{
-			href: '/lists/all',
-			title: 'all'
-		},
-		{
-			href: '/lists/inbox',
-			title: 'inbox'
-		}
-	];
-
-	let { data, children } = $props();
+	let { children } = $props();
 </script>
 
 <div class="flex w-full flex-row">
@@ -36,60 +17,5 @@
 			<Timer class="h-4 w-4" />
 		</a>
 	</nav>
-
-	<nav class="flex min-w-[200px] flex-col border-r px-2 py-2">
-		<a
-			href="/"
-			aria-current={$page.url.pathname === '/'}><span>Home</span></a>
-		{#snippet item({ title, href }: PremadeListItem)}
-			<a
-				class="capitalize"
-				aria-current={$page.url.pathname.includes(href)}
-				href={href}>
-				<span>{title}</span>
-			</a>
-		{/snippet}
-
-		{#each premadeLists as premadeItem}
-			{@render item(premadeItem)}
-		{/each}
-
-		{#each data.lists as l}
-			{@render item({ title: l.title, href: `/lists/${l.id}` })}
-		{/each}
-
-		<div class="divider"></div>
-
-		<form
-			class="mt-2 flex flex-row space-x-2"
-			action="/lists"
-			method="post">
-			<Input name="title" />
-			<Button
-				variant="outline"
-				size="icon"
-				type="submit"><Plus /></Button>
-		</form>
-	</nav>
-
 	{@render children()}
 </div>
-
-<style lang="postcss">
-	nav > a {
-		@apply no-underline [@media(hover:hover)]:hover:underline;
-		&:hover {
-			@apply cursor-pointer underline;
-		}
-	}
-	.divider {
-		@apply flex flex-row items-center self-stretch;
-		&:before,
-		&:after {
-			@apply h-0.5 w-full flex-grow content-[''];
-		}
-	}
-	[aria-current='true'] {
-		@apply text-center underline;
-	}
-</style>
