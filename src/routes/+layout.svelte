5c3fb1e776d4ca1fb3094c709a1378181f07d39d
Emanuel <152207479+eeyieryi@users.noreply.github.com>
2024-09-28 21:46:18 +0400

    nav different "apps"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 
 	import { page } from '$app/stores';
 
-	import { Plus } from 'lucide-svelte';
+	import { Plus, ListTodo, Timer } from 'lucide-svelte';
 
 	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button';
@@ -28,11 +28,26 @@
 </script>
 
 <div class="flex w-full flex-row">
+	<nav class="flex w-full max-w-[29px] flex-col items-center space-y-2 border-r px-0.5 pt-2">
+		<a href={$page.url.pathname.startsWith('/lists') ? '#' : '/lists'}>
+			<ListTodo class="h-4 w-4" />
+		</a>
+		<a href="/timer">
+			<Timer class="h-4 w-4" />
+		</a>
+	</nav>
+
 	<nav class="flex min-w-[200px] flex-col border-r px-2 py-2">
-		<a href="/" aria-current={$page.url.pathname === '/'}><span>Home</span></a>
+		<a
+			href="/"
+			aria-current={$page.url.pathname === '/'}><span>Home</span></a>
 		{#snippet item({ title, href }: PremadeListItem)}
-			<a class="capitalize" aria-current={$page.url.pathname.includes(href)} {href}
-				><span>{title}</span></a>
+			<a
+				class="capitalize"
+				aria-current={$page.url.pathname.includes(href)}
+				href={href}>
+				<span>{title}</span>
+			</a>
 		{/snippet}
 
 		{#each premadeLists as premadeItem}
@@ -45,9 +60,15 @@
 
 		<div class="divider"></div>
 
-		<form class="mt-2 flex flex-row space-x-2" action="/lists" method="post">
+		<form
+			class="mt-2 flex flex-row space-x-2"
+			action="/lists"
+			method="post">
 			<Input name="title" />
-			<Button variant="outline" size="icon" type="submit"><Plus /></Button>
+			<Button
+				variant="outline"
+				size="icon"
+				type="submit"><Plus /></Button>
 		</form>
 	</nav>
 
